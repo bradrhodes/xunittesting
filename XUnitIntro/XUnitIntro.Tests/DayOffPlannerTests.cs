523ab657a5930d33227e32200ed9760bf014a83a
Brad Rhodes <bradr@iqmetrix.com>
2016-05-27 14:08:16 -0600

    Added DayOffPlanner and a test.

---
+++ XUnitIntro/XUnitIntro.Tests/DayOffPlannerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace XUnitIntro.Tests
+{
+    public class DayOffPlannerTests
+    {
+	    public class WhenDecidingWhatToDo
+	    {
+		    [Fact]
+		    public void ItShouldTellMeToStayInside()
+		    {
+			    // arrange
+				var sut = new DayOffPlanner();
+
+				// act
+			    var result = sut.WhatShouldIDoToday();
+
+				// assert
+				Assert.Equal(Activity.StayInside, result);
+		    }
+	    }
+    }
+}
---
+++ XUnitIntro/XUnitIntro/DayOffPlanner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XUnitIntro
+{
+	public class DayOffPlanner
+	{
+		public Activity WhatShouldIDoToday()
+		{
+			return Activity.StayInside;
+		}
+	}
+
+	public enum Activity
+	{
+		GoOutside,
+		StayInside
+	}
+}
